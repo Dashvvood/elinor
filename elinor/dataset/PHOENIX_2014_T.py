2bf23fe289fe3aee4e5cef6cff87fab10cdbd350
Dashvvood <780966523@qq.com>
2025-04-07 08:11:46 +0800

    refactor(PHOENIX_2014_T.py): novacula Occami

--- elinor/dataset/PHOENIX_2014_T.py
+++ elinor/dataset/PHOENIX_2014_T.py
@@ -2,10 +2,25 @@
 import os
 from pathlib import Path
 import pandas as pd
-from typing import List, Optional
+from typing import List, Generator
 from elinor import count_files_by_end
 import re
 from PIL import Image
+# from dataclasses import dataclass
+from dataclasses import dataclass
+from string import Template
+
+
+@dataclass
+class BaseDatasetPhoenix2014TItem:
+    frames: Generator
+    num: int
+    orth: str
+    translation: str
+
+TemplateFeatureDir = Template("${root_dir}/features/fullFrame-210x260px/${split}")
+TemplateAnnotation = Template("${root_dir}/annotations/manual/PHOENIX-2014-T.${split}.corpus.csv")
+TemplateFrame = Template("images${frame}.png")
 
 class BaseDatasetPhoenix2014T(object):
     """Phoenix-2014-T数据集基础类
@@ -32,90 +47,45 @@ class BaseDatasetPhoenix2014T(object):
             split="dev" # train, test, dev
         ):
         self.root_dir=root_dir
-        self.features_dir = os.path.join(self.root_dir, "features/fullFrame-210x260px", split)
-        self.annotations_dir = os.path.join(self.root_dir, "annotations", "manual",)
-        self.annoatation_df = pd.read_csv(
-            os.path.join(self.annotations_dir, f"PHOENIX-2014-T.{split}.corpus.csv"),
-            sep="|"
-        ).rename(columns={"name": "dirname"})
-        self._self_check_features_dir()
-        self.feature_df = self._generate_feature_df()
+        self.features_dir = TemplateFeatureDir.substitute(root_dir=self.root_dir,split=split)
+        self.annotations_path = TemplateAnnotation.substitute(root_dir=self.root_dir, split=split)
+        self.metadata = self.generate_metadata_from_annotations()
 
 
-    def _get_annotation_by_dirname(self, dirname):
-        return self.annoatation_df[self.annoatation_df["dirname"] == dirname]
+    def __len__(self):
+        return len(self.metadata)
     
 
-    def _self_check_features_png(self):
-        dirs_to_check = [
-            self.root_dir,
-            self.features_dir,
-            self.annotations_dir,
-        ]
-        for directory in dirs_to_check:
-            if not os.path.isdir(directory):
-                raise FileNotFoundError(f"Directory does not exist: {directory}")
+    def __getitem__(self, index):
+        metadata = self.metadata.iloc[index]
+        frames = self.get_frames_by_dirname(metadata.dirname)
+        num = self.metadata.iloc[index].num
+
+        return BaseDatasetPhoenix2014TItem(
+            frames=frames,
+            num=num,
+            orth=metadata.orth,
+            translation=metadata.translation
+        )
+
+
+    def generate_metadata_from_annotations(self):
+        metadata = pd.read_csv(self.annotations_path, sep="|").rename(columns={"name": "dirname"})
         
-        total_files = count_files_by_end(directory=self.features_dir, ends=".png")
-        return total_files
-    
-
-    def _self_check_features_dir(self):
-        subdirs = set()
-        for root, dirs, files in os.walk(self.features_dir):
-            for d in dirs:
-                subdirs.add(d)
-        # Check if the subdirectories match the names in the annotation DataFrame
-        assert subdirs == set(self.annoatation_df.dirname.values), f"Subdirectories in {self.features_dir} do not match annotation names."
-        return subdirs
-
-
-    def _generate_feature_df(self):
-        metadata_list = []
-        metadata = {
-            "dirname": None,
-            "frame": None,
-            "image": None,
-        }
-        pattern = r'(\d+)'
-        
-        for dirname in self.annoatation_df.dirname:
+        frame_nums = []
+        for dirname in metadata.dirname:
+            # Check if the directory exists
             dir_path = os.path.join(self.features_dir, dirname)
-            files = sorted([f for f in os.listdir(dir_path) if f.endswith('.png')])
-            for file in files:
-                # Extract the frame number from the filename
-                frame_number = int(re.search(pattern, file).group(1))
-                metadata["dirname"] = dirname
-                metadata["frame"] = frame_number
-                metadata["image"] = file
-                metadata_list.append(metadata.copy())
-        # Create a DataFrame from the metadata list
-        metadata_df = pd.DataFrame(metadata_list)
+            frame_num = len([f for f in os.listdir(dir_path) if f.endswith('.png')])
+            frame_nums.append(frame_num)
+        
+        metadata["num"] = frame_nums
+        return metadata 
+    
 
-        return metadata_df
-
-    def get_all_features_path_by_dirname(self, dirname):
-        """
-        Get all features by name
-        Args:
-            name: The name of the feature
-        Returns:
-            A DataFrame containing all features with the specified name
-        """
-        data = self.feature_df[self.feature_df["dirname"] == dirname].sort_values(by="frame").image
-        return [os.path.join(self.features_dir, dirname, file) for file in data]
-
-    def get_all_features_generator_by_dirname(self, dirname):
-        """
-        Get all features by name
-        Args:
-            name: The name of the feature
-        Returns:
-            A DataFrame containing all features with the specified name
-        """
-        data = self.get_all_features_path_by_dirname(dirname)
-        for i, path in enumerate(data):
-            yield Image.open(path)
-
-if __name__ == '__main__':
-    dataset = BaseDatasetPhoenix2014T()
+    def get_frames_by_dirname(self, dirname) -> Generator:
+        video_path = os.path.join(self.features_dir, dirname)
+        frame_num = self.metadata[self.metadata["dirname"] == dirname].num.item()
+        for i in range(frame_num):
+            frame_path = os.path.join(video_path, TemplateFrame.substitute(frame=f"{i+1:04d}"))
+            yield Image.open(frame_path)
