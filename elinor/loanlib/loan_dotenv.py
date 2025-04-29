1e9acea2e4f590e04e1f9df71d8b0180dbcebb6a
Dashvvood <780966523@qq.com>
2025-04-29 21:08:01 +0800

    fix(loadlib): use simplenamespace to improve loan_dotenv

--- elinor/loanlib/loan_dotenv.py
+++ elinor/loanlib/loan_dotenv.py
@@ -2,16 +2,21 @@ import os
 import sys
 from typing import List
 from dotenv import load_dotenv
+from types import SimpleNamespace
 
 def fast_loadenv_then_append_path(vars:List[str] = ["PROJECT_ROOT"]):
     load_dotenv()
+    ns = SimpleNamespace(**{k: None for k in vars}) 
+
     for var in vars:
         if var in os.environ:
             path = os.environ[var]
+            ns.__setattr__(var, path)
             if os.path.exists(path):
                 sys.path.append(path)
             else:
                 raise FileNotFoundError(f"Path {path} does not exist.")
         else:
             raise KeyError(f"Environment variable {var} not found.")
-
+        
+    return ns
