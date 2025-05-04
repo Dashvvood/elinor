fab75940c3a11d14b2b9729bcf5be6cae891e961
Dashvvood <780966523@qq.com>
2025-05-05 06:14:42 +0800

    fix(loan_dotenv): verbose

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="elinor",
-    version="0.0.9",
+    version="0.0.10",
     author="Dashvvood",
     author_email="mathismottis@gmail.com",
     description="some util functions",
