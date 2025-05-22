7b85059ccbf89e76d635990f971c4405d45ab7dc
Dashvvood <780966523@qq.com>
2025-05-22 08:28:44 +0800

    feat(dataset/PHOENIX2014t): improve structure
    🚧

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="elinor",
-    version="0.0.11",
+    version="0.0.12",
     author="Dashvvood",
     author_email="mathismottis@gmail.com",
     description="some util functions",
