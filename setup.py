8db8edef6acdaadf9b809ed8a5fb428f4d2aa119
Dashvvood <780966523@qq.com>
2025-05-29 16:52:59 +0800

    :bug:

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="elinor",
-    version="0.0.14",
+    version="0.0.15",
     author="Dashvvood",
     author_email="mathismottis@gmail.com",
     description="some util functions",
