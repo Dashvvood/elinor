433495472d9022ac33700a31873d6d5269a69c8a
Dashvvood <780966523@qq.com>
2025-07-15 14:08:44 +0800

    :art: test action

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="elinor",
-    version="0.0.15",
+    version="0.0.16",
     author="Dashvvood",
     author_email="mathismottis@gmail.com",
     description="some util functions",
